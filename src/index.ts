3b9f3690c5fe2d98d8404343fe352ab6e832b08f
CamilleAbella <camille.abella@hotmail.fr>
2020-09-19 23:48:45 +0200

    continue

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import yargs from "yargs"
+
+/**
+ * nano-bot create
+ * --name [name]
+ * --nano-modules [module1 [module2 [...]]]
+ * nano-module install [module1 [module2 [...]]]
+ * nano-module remove [module1 [module2 [...]]]
+ * nano-module create
+ */
